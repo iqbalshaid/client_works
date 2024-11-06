04753c0e00f559ab09492529d20f0b8f9b6200dc
md shahid iqbal <111365870+iqbalshaid@users.noreply.github.com>
2024-11-06 23:42:20 +0530

    Update Foodprocessing.js

--- src/components/Our Domain/Foodprocessing.js
+++ src/components/Our Domain/Foodprocessing.js
@@ -64,17 +64,17 @@ const FoodProcessing = () => {
         <Slider {...settings}>
           {images.map((src, index) => (
             <div key={index} style={{ textAlign: "center" }}>
-              <img
-                src={src}
-                alt={Slide ${index}}
-                style={{
-                  width: "100%",
-                  height: "400px",
-                  borderRadius: "8px",
-                  objectFit: "contain",
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                }}
-              />
+              <img 
+  src={src} 
+  alt={`Slide ${index}`} 
+  style={{ 
+    width: "100%",
+    height: "400px",
+    borderRadius: "8px",
+    objectFit: "contain",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+  }} 
+/>
               <h3
                 style={{
                   marginTop: "10px",
